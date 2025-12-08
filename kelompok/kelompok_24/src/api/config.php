1675f27404d598e8dc22384120780fd0cbab7e4d
rendy antono <rendyant2305@gmail.com>
2025-12-08 14:23:54 +0700

    Add file configphp

---
+++ kelompok/kelompok_24/src/api/config.php
@@ -0,0 +1,21 @@
+<?php
+// src/config.php
+
+// ----------------------------------------
+// KONFIGURASI DATABASE
+// Ganti nilai-nilai berikut sesuai dengan pengaturan database Anda
+// ----------------------------------------
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'warkops_db');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Karakter set default
+define('DB_CHARSET', 'utf8mb4');
+
+// ----------------------------------------
+// KONFIGURASI APLIKASI UMUM (Opsional)
+// ----------------------------------------
+// define('APP_NAME', 'Warkop POS Management');
+// define('BASE_URL', 'http://localhost/nama_project/');
+?>

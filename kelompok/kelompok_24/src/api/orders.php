42d343d340ea2d85a00c31e12bec5d6435ecba58
hino89 <cendol3409@gmail.com>
2025-12-10 00:48:22 +0700

    fix php file linkage ../config/dn.php to config/db.php

--- kelompok/kelompok_24/src/api/orders.php
+++ kelompok/kelompok_24/src/api/orders.php
@@ -6,6 +6,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+require_once 'config.php';
 require_once "db.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
